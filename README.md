b659be3ff8c39f099353a936a29b3bc650edccbb
Dan Vega <danvega@gmail.com>
2024-05-16 12:12:52 -0400

    Adding post tutorial cleanup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Open AI GPT-4o + Java
+
+This is just a simple demo that shows how to communicate with the Open AI API. If you want to watch a detailed 
+explanation you check out the tutorial on YouTube below. 
+
+[YouTube Tutorial](https://youtu.be/EDJLHWcFvpQ)
--- src/main/java/dev/danvega/Application.java
+++ src/main/java/dev/danvega/Application.java
@@ -1,2 +1,38 @@
-package dev.danvega;public class Application {
+package dev.danvega;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var apiKey = System.getenv("OPENAI_API_KEY");
+        var body = """
+                {
+                    "model": "gpt-4o",
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": "Tell me a good dad joke about cats"
+                        }
+                    ]
+                }""";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.openai.com/v1/chat/completions"))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + apiKey)
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        var client = HttpClient.newHttpClient();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+
+
 }
